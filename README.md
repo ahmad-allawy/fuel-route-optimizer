d23c9dbb4856da93e160bf6ecc589bc9e9c5d0fa
a-ilawy <mahmed20156947@gmail.com>
2026-02-28 22:30:22 +0200

    Remove old root files after moving to backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# fuel-route-optimizer
-Django API that calculates optimal fuel stops along a US route based on cost efficiency and vehicle range constraints.
--- routing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from routing.views import RouteFuelAPIView
-
-urlpatterns = [
-    path('route-fuel/',RouteFuelAPIView.as_view(), name='route-fuel'),
-]
